bb00866e8783ea65b41d62aad80f5f9f6becd1b4
Raffaello Ciarelli <63007734+raffociar05@users.noreply.github.com>
2024-04-04 11:32:11 +0200

    Update LSTM Temp medie.py

--- LSTM Temp medie.py
+++ LSTM Temp medie.py
@@ -1,146 +1,176 @@
-#Importazione librerie
-
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
-from tensorflow.keras.callbacks import EarlyStopping
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from keras.models import Sequential
+from keras.layers import LSTM, Dropout, Dense
+from keras.callbacks import EarlyStopping
+from keras.utils import plot_model
+import tensorflow as tf
 
-# Caricamento del dataframe
-df = pd.read_excel("df_temp_media_bis.xlsx")
+#importazione dataframe originario
+df_original = pd.read_excel('df_temp_media_bis.xlsx')
+df_original['Data'] = pd.to_datetime(df_original['Data'])
+df_original.set_index('Data', inplace=True)
+df_original.describe()
 
-# Visualizzazione delle prime righe
-print(df.head())
+# Creazione del grafico originario
+plt.figure(figsize=(12, 7))
+plt.plot(df_original['Gradi'], color='black')
+plt.xlabel('Anni')
+plt.ylabel('Temperatura (°C)')
+plt.title('Andamento della Temperatura Media Italiana')
+plt.legend()
+plt.grid(True)
+plt.show()
 
-# Preprocessing dei dati
+# importazione dataframe
+df = pd.read_excel('df_temp_media_bis.xlsx')
+#Preprocessing
 
-# Conversione della variabile "Data" in formato datetime
+#Conversione della variabile 'Data' nel formato datetime
+#e impostazione della data come indice
 df['Data'] = pd.to_datetime(df['Data'])
+df.set_index('Data', inplace=True)
 
-'''
-Quando si usa reshape(-1, 1), si chiede a numpy di rimodellare l'array
-con 1 colonna e tante righe quante sono necessarie per contenere i dati.
-Questa operazione darà come risultato un array 2D di forma (n, 1),
-dove n è il numero di elementi dell'array originale.
-'''
+#Standardizzazione della variabile "Gradi"
+scaler = StandardScaler()
+df['Gradi'] = scaler.fit_transform(df[['Gradi']])
 
-data = df['Gradi'].values.reshape(-1, 1)
+#Suddivisione del dataframe in training e test set
+train_size = int(len(df) * 0.8)
+test_size = len(df) - train_size
+train, test = df.iloc[0:train_size], df.iloc[train_size:len(df)]
 
-# Normalizzazione dei dati tra i valori 0 e 1
-scaler = MinMaxScaler(feature_range=(0, 1))
-data_normalized = scaler.fit_transform(data)
+# Creazione del grafico train/test
+plt.figure(figsize=(12, 7))
+plt.plot(train['Gradi'], color='black', label='Training Set')
+plt.plot(test['Gradi'], color='red', label='Test Set')
+plt.xlabel('Anni')
+plt.ylabel('Temperatura (°C)')
+plt.title('Andamento delle Temperatura media italiana')
+plt.legend()
+plt.grid(True)
+plt.show()
 
-# Creazione delle sequenze temporali
-def create_sequences(data, seq_length):
-    X, y = [], []
-    for i in range(len(data) - seq_length):
-        X.append(data[i:i+seq_length])
-        y.append(data[i+seq_length])
-    return np.array(X), np.array(y)
+#Creazione dataset per la rete LSTM
+def create_dataset(X, time_steps=1):
+    Xs, ys = [], []
+    for i in range(len(X) - time_steps):
+        v = X.iloc[i:(i + time_steps)].values
+        Xs.append(v)
+        ys.append(X.iloc[i + time_steps])
+    return np.array(Xs), np.array(ys)
 
-# Definizione della lunghezza della sequenza temporale
-seq_length = 30
+time_steps = 30
+X_train, y_train = create_dataset(train, time_steps)
+X_test, y_test = create_dataset(test, time_steps)
 
-# Creazione delle sequenze temporali
-X, y = create_sequences(data_normalized, seq_length)
-
-# Divisione dei dati in set di addestramento e di test
-train_size = int(len(X) * 0.7)
-X_train, X_test = X[:train_size], X[train_size:]
-y_train, y_test = y[:train_size], y[train_size:]
-
-# Definizione del modello LSTM
+#Costruzione e compilazione modello LSTM
 model = Sequential()
-model.add(LSTM(units=50, activation='relu', return_sequences=True,input_shape=(seq_length, 1)))
-# Secondo layer LSTM
-model.add(LSTM(units=50, return_sequences=True))
-# Terzo layer LSTM
-model.add(LSTM(units=50))
-# Layer completamente connesso
-model.add(Dense(units=1))
-
-# Compilazione del modello
+model.add(LSTM(64, activation='tanh', return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])))
+model.add(Dropout(0.2))
+model.add(LSTM(32, activation='tanh', return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(16, activation='tanh', return_sequences=False))
+model.add(Dense(1))
 model.compile(optimizer='adam', loss='mean_squared_error')
 
-# Addestramento del modello
-early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
-history = model.fit(X_train, y_train, epochs=100, batch_size=32, validation_split=0.2, callbacks=[early_stopping])
 
+#Early Stopping
+early_stop = EarlyStopping(monitor='val_loss', patience=10)
+#Addestramento del modello
+history = model.fit(X_train, y_train,
+                    epochs=100,
+                    batch_size=32,
+                    validation_split=0.1,
+                    callbacks=[early_stop],
+                    shuffle=False)
 
+#salvataggio del modello LSTM
+model.save('model_LSTM.keras')
+#caricamento del modello LSTM
+model = tf.keras.saving.load_model('model_LSTM.keras')
 
-model.save('lstm_model.keras')
-
+#stampa del modello LSTM
 model.summary()
-
-#!pip install graphviz
-from keras.utils import plot_model
 plot_model(model)
 
-# Visualizzazione dell'andamento della loss durante l'addestramento
+#Grafico funzione di loss train/test
 plt.plot(history.history['loss'], label='Training Loss')
 plt.plot(history.history['val_loss'], label='Validation Loss')
-plt.xlabel('Epochs')
+plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.legend()
 plt.show()
 
+#
+predictions = model.predict(X_test)
+predictions = scaler.inverse_transform(predictions)
+y_test = scaler.inverse_transform(y_test)
 
+test
+# Creazione array di date (19-10-2020 / 31-12-2023)
+date = pd.date_range(start='2020-10-19', end='2023-12-31')
 
-# Valutazione del modello
-train_loss = model.evaluate(X_train, y_train, verbose=0)
-test_loss = model.evaluate(X_test, y_test, verbose=0)
-print(f'Train Loss: {train_loss}')
-print(f'Test Loss: {test_loss}')
+# Controllo lunghezza array tra y_test e predictions
+if len(date) != len(y_test):
+    date = date[:len(y_test)]
 
+#Grafico: confronto valori predetti (fitted) e valori reali (test set)
+plt.figure(figsize=(10, 6))
+plt.plot(date, y_test, color='blue', label='Dati reali')
+plt.plot(date, predictions, color='red', label='Dati predetti')
+plt.legend(loc='upper right')
+plt.title('Confronto tra dati reali e predetti')
+plt.xlabel('Anni')
+plt.ylabel('Gradi (°C)')
+plt.grid(True)
 
-'''
-I risultati che si ottengono dall'addestramento della rete LSTM mostrano le perdite sul training set e sul test set.
-La perdita, spesso chiamata errore di training o errore di test, è una misura della discrepanza tra i valori previsti dal 
-modello e i valori reali nei dati di training e di test, rispettivamente.
-Train Loss (Perdita sul training set): La perdita sul training set è la misura dell'errore del modello sui dati che sono 
-stati utilizzati per addestrare il modello stesso. Una bassa perdita sul training set indica che il modello è stato in grado
-di adattarsi bene ai dati di training e ha imparato a rappresentarli efficacemente.
-Test Loss (Perdita sul test set): La perdita sul test set è la misura dell'errore del modello sui dati che non sono stati 
-utilizzati durante l'addestramento, ma sono stati riservati appositamente per valutare le prestazioni del modello su dati non visti.
-Una bassa perdita sul test set indica che il modello è in grado di generalizzare bene oltre ai dati di training e può fare
-previsioni accurate su nuovi dati.
-In questo caso, si ha una perdita leggermente maggiore sul test set rispetto al training set. 
-Tuttavia, le differenze tra la perdita sul training set e quella sul test set sembrano essere relativamente piccole, il che 
-potrebbe indicare che il modello sta generalizzando bene e non sta soffrendo di overfitting (cioè, non sta adattando troppo 
-strettamente i dati di training).
-In generale, questi risultati indicano che il modello ha imparato a rappresentare efficacemente i dati di training e a 
-generalizzare bene su nuovi dati, il che è un segno positivo della sua capacità di fare previsioni accurate.
-'''
-
-
-# Previsione sui prossimi 3 anni
-future_predictions = []
-last_sequence = X[-1]
-for _ in range(365 * 3):  # 3 anni
-    prediction = model.predict(last_sequence.reshape(1, seq_length, 1))
-    future_predictions.append(prediction[0, 0])
-    last_sequence = np.roll(last_sequence, -1)
-    last_sequence[-1] = prediction
-
-# Denormalizzazione delle previsioni
-future_predictions_denormalized = scaler.inverse_transform(np.array(future_predictions).reshape(-1, 1))
-
-
-
-# Plot delle previsioni
-plt.plot(df['Data'], df['Gradi'], label='Dati Storici')
-plt.plot(pd.date_range(start='2024-01-01', periods=len(future_predictions_denormalized), freq='D'), future_predictions_denormalized, label='Previsioni Future', linestyle='--')
-plt.xlabel('Data')
-plt.ylabel('Temperatura (°C)')
-plt.title('Previsione delle Temperature Future')
-plt.legend()
 plt.show()
+predicted_grades = model.predict(X_test)
 
-print(future_predictions_denormalized)
+#inverione della conversione per riportare i dati ai valori originari
+predicted_grades_inverse = scaler.inverse_transform(predicted_grades)
+predicted_grades_inverse_df = pd.DataFrame(predicted_grades_inverse)
 
-future_predictions_denormalized_df = pd.DataFrame(future_predictions_denormalized)
-future_predictions_denormalized_df.to_csv('future_predictions_denormalized.csv')
+predicted_grades_inverse_df.describe()
 
-df['Data']
+#date future per la previsione
+new_date = pd.date_range(start='2024-01-01', end='2027-02-12')
+
+predicted_grades_inverse_df['Data'] = new_date
+predicted_grades_inverse_df
+#rinominazione della variabile "Gradi"
+predicted_grades_inverse_df = predicted_grades_inverse_df.rename(columns={predicted_grades_inverse_df.columns[0]: 'Gradi'})
+
+predicted_grades_inverse_df
+#riodinamento colonne nel df
+cols = list(predicted_grades_inverse_df.columns)
+cols[0], cols[1] = cols[1], cols[0]
+predicted_grades_inverse_df = predicted_grades_inverse_df.reindex(columns=cols)
+
+predicted_grades_inverse_df
+predicted_grades_inverse_df.describe()
+df_original = pd.DataFrame(df_original)
+df_original.describe()
+
+#nuovo datafame con valori originari e previsioni
+df_new = pd.concat([df_original, predicted_grades_inverse_df], ignore_index=True)
+df_new
+df_original = pd.read_excel('df_temp_media_bis.xlsx')
+
+# Creazione del grafico originario + previsioni
+plt.figure(figsize=(12, 7))
+plt.plot(df_original['Data'], df_original['Gradi'], linestyle='-', color='gray', label='Dati originari')
+plt.plot(predicted_grades_inverse_df['Data'], predicted_grades_inverse_df['Gradi'], linestyle='-', color='red', label='Previsioni future')
+plt.xlabel('Anni')
+plt.ylabel('Gradi (°C)')
+plt.title('Temperatura media italiana')
+plt.grid(True)
+plt.xticks(rotation=45)
+plt.legend() #legenda
+
+plt.show()
