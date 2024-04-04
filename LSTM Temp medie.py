45731eb179eb430f7399c9e16b1fb32de7330c0d
Raffaello Ciarelli <63007734+raffociar05@users.noreply.github.com>
2024-04-04 11:19:46 +0200

    Update LSTM Temp medie.py

--- LSTM Temp medie.py
+++ LSTM Temp medie.py
@@ -8,7 +8,7 @@ from tensorflow.keras.layers import LSTM, Dense
 from tensorflow.keras.callbacks import EarlyStopping
 import matplotlib.pyplot as plt
 
-# caricamento del dataframe
+# Caricamento del dataframe
 df = pd.read_excel("df_temp_media_bis.xlsx")
 
 # Visualizzazione delle prime righe
